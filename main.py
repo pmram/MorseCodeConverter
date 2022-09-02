a38f234792cb705c08b49a1cd8317a43ec690076
wolfsoad <pmramaia.engmec@gmail.com>
2022-09-02 18:34:17 +0100

    Update main.py to include exception handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import werkzeug
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from errorHandling import InvalidUsage
 from morseDictionary import MorseDictionary, morseconverter
 
 app = Flask(__name__)
@@ -13,10 +13,19 @@ def hello():
 @app.route("/convert/")
 def convert():
     string = request.args.get('text')
+    if string is None:
+        raise InvalidUsage('This view is gone', status_code=410)
     response = {"result": {
         "original": string,
         "morse": morseconverter(string)
     }}
+    return response
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
     return response
 
 
